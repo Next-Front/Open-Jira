70f71628c3c896cae4acaf1612e7df1c23c0f796
Kevin cuadros <kevindavidcuadros@gmail.com>
2022-06-10 17:25:31 -0500

    add select for status

--- components/ui/EntryCard.tsx
+++ components/ui/EntryCard.tsx
@@ -1,25 +1,41 @@
-import { FC } from 'react'
-import { Card, CardActionArea, CardActions, CardContent, Typography } from '@mui/material';
-import { Entry } from '../../interfaces/entry';
+import { FC, useContext, useState } from 'react'
+import { Card, CardActionArea, CardActions, CardContent, MenuItem, Select, SelectChangeEvent, Typography } from '@mui/material';
+import { Entry, EntryStatus } from '../../interfaces/entry';
+import { EntriesContext } from '../../context/entries/EntriesContext';
 
 interface Props {
   entry: Entry
 }
 
-const EntryCard: FC<Props> = ({ entry: { description, _id, createdAt } }) => {
+const EntryCard: FC<Props> = ({ entry: { description, _id, createdAt, status } }) => {
+
+  const [activeDrag, setActiveDrag] = useState(false)
+  const [valueSelect, setValueSelect] = useState(status)
+  const { updateEntryStatus }  = useContext(EntriesContext)
 
   const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
     event.dataTransfer.setData('text', _id);
+    setActiveDrag(true)
   }
 
   const onDragEnd = (event: React.DragEvent<HTMLDivElement>) => {
     console.log(event);
+    setActiveDrag(false)
+  }
+
+  const onSelectChange = (event: SelectChangeEvent<"pending" | "in-progress" | "done">) => {
+    setValueSelect(event.target.value as EntryStatus)
+    updateEntryStatus(_id, event.target.value as EntryStatus)
   }
 
   return (
     <Card
       draggable
-      sx={{ marginBottom: 1 }}
+      sx={{ 
+        marginBottom: 1,
+        background: activeDrag ? '#494352' : '',
+        opacity: 1,
+      }}
       onDragStart={onDragStart}
       onDragEnd={onDragEnd}
     >
@@ -27,7 +43,16 @@ const EntryCard: FC<Props> = ({ entry: { description, _id, createdAt } }) => {
         <CardContent>
           <Typography sx={{ whiteSpace: 'pre-line' }} >{ description }</Typography>
         </CardContent>
-        <CardActions sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Select
+            value={valueSelect}
+            sx={{ width: 'auto', height: "30px" }}
+            onChange={onSelectChange}
+          >
+            <MenuItem value="pending">Pending</MenuItem>
+            <MenuItem value="in-progress">In Progress</MenuItem>
+            <MenuItem value="done">Done</MenuItem>
+          </Select>
           <Typography variant='body2' >hace 30 minutos</Typography>
         </CardActions>
       </CardActionArea>
