6dbe730397a2222a3df47c508ffdfe366d3726f7
Kevin cuadros <kevindavidcuadros@gmail.com>
2022-06-22 15:50:39 -0500

    catch error connect

--- database/db.ts
+++ database/db.ts
@@ -11,27 +11,42 @@ const mongoConnection = {
 }
 
 export const connect = async () => {
-  if (mongoConnection.isConnected === 1) return;
+  
+  try {   
 
-  if( mongoose.connections.length > 0 ) {
-    mongoConnection.isConnected = mongoose.connections[0].readyState;
-    if (mongoConnection.isConnected === 1) return;
-    await mongoose.disconnect();
+      if (mongoConnection.isConnected === 1) return;
+      if( mongoose.connections.length > 0 ) {
+        mongoConnection.isConnected = mongoose.connections[0].readyState;
+        if (mongoConnection.isConnected === 1) return;
+        await mongoose.disconnect();
+      }
+      await mongoose.connect( process.env.MONGO_URL!)
+      mongoConnection.isConnected = 1;
+
+  } catch (error) {
+    console.log({
+      error,
+      function: 'connect'
+    })
   }
 
-  await mongoose.connect( process.env.MONGO_URL! , {
-
-  })
-  mongoConnection.isConnected = 1;
 
 }
 
 export const disconnect = async () => {
 
-  if( process.env.NODE_ENV === 'development') return;
-
-  if (mongoConnection.isConnected == 0) return;
-  await mongoose.disconnect();
-  console.log('disconnected');
+  try {
+    
+    if( process.env.NODE_ENV === 'development') return;
   
+    if (mongoConnection.isConnected == 0) return;
+    await mongoose.disconnect();
+    mongoConnection.isConnected = 0;
+    console.log('disconnected');
+  } catch (error) {
+    console.log({
+      error,
+      function: 'disconnect'
+    })
+  }
 }
